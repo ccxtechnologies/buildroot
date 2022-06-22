9fa7d1d5ba293ac229cf3488681338be964db74c
tamara <tamara@ccxtechnologies.com>
2022-06-22 17:46:35 -0400

    pull in systemd from support-v1.6.x

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-SYSTEMD_VERSION = 246.6.ccx2
-SYSTEMD_SITE = $(call github,ccxtechnologies,systemd,v$(SYSTEMD_VERSION))
+SYSTEMD_VERSION = 26f5a6d
+SYSTEMD_SITE = $(call github,ccxtechnologies,systemd,$(SYSTEMD_VERSION))
 SYSTEMD_LICENSE = LGPL-2.1+, GPL-2.0+ (udev), Public Domain (few source files, see README), BSD-3-Clause (tools/chromiumos)
 SYSTEMD_LICENSE_FILES = LICENSE.GPL2 LICENSE.LGPL2.1 README tools/chromiumos/LICENSE
 SYSTEMD_INSTALL_STAGING = YES
