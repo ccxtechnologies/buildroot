47ef71874a6904f7eda3c1f563319f4826016029
tamara <tamara@ccxtechnologies.com>
2025-01-06 13:08:27 -0500

    uprev netperf for license

--- package/netperf/netperf.mk
+++ package/netperf/netperf.mk
@@ -4,16 +4,20 @@
 #
 ################################################################################
 
-NETPERF_VERSION = 2.7.0
-NETPERF_SITE = http://sources.buildroot.org/netperf
-NETPERF_SOURCE = netperf-$(NETPERF_VERSION).tar.bz2
+NETPERF_VERSION = 3bc455b
+NETPERF_SITE = $(call github,HewlettPackard,netperf,$(NETPERF_VERSION))
 # gcc 5+ defaults to gnu99 which breaks netperf
 NETPERF_CONF_ENV = \
 	ac_cv_func_setpgrp_void=set \
 	CFLAGS="$(TARGET_CFLAGS) -std=gnu89"
 NETPERF_CONF_OPTS = --enable-demo=yes
-NETPERF_LICENSE = netperf license
+NETPERF_LICENSE = MIT
 NETPERF_LICENSE_FILES = COPYING
+
+define NETPERF_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+NETPERF_PRE_CONFIGURE_HOOKS += NETPERF_RUN_AUTOGEN
 
 define NETPERF_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/src/netperf \
