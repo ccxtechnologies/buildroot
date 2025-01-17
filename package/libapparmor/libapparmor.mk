d80e6bcaf76a420c7953ac3904ea1535bb9bfa53
tamara <tamara@ccxtechnologies.com>
2025-01-17 16:52:18 -0500

    add cpe ids

--- package/libapparmor/libapparmor.mk
+++ package/libapparmor/libapparmor.mk
@@ -11,6 +11,8 @@ LIBAPPARMOR_SOURCE = apparmor-v$(LIBAPPARMOR_VERSION).tar.gz
 LIBAPPARMOR_SITE = https://gitlab.com/apparmor/apparmor/-/archive/v$(LIBAPPARMOR_VERSION)
 LIBAPPARMOR_LICENSE = LGPL-2.1
 LIBAPPARMOR_LICENSE_FILES = LICENSE libraries/libapparmor/COPYING.LGPL
+LIBAPPARMOR_CPE_ID_VENDOR = apparmor
+LIBAPPARMOR_CPE_ID_PRODUCT = apparmor
 
 LIBAPPARMOR_DEPENDENCIES = host-bison host-flex host-pkgconf
 LIBAPPARMOR_SUBDIR = libraries/libapparmor
