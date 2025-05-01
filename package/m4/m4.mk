66140ee319b729a7cabeca14a2c786c65a66f87f
tamara <tamara@ccxtechnologies.com>
2025-05-01 10:47:33 -0400

    add temporary m4 host fix

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -10,4 +10,7 @@ M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_LICENSE = GPL-3.0+
 M4_LICENSE_FILES = COPYING
 
+# This should be removed with 1.4.20
+HOST_M4_CONF_ENV = CFLAGS="$(HOST_CFLAGS) -std=gnu17"
+
 $(eval $(host-autotools-package))
