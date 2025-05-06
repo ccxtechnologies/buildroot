1e44d58202976e662da179ae24efe1a29e420b50
tamara <tamara@ccxtechnologies.com>
2025-05-06 14:22:21 -0400

    use older compatible standard

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,16 +4,18 @@
 #
 ################################################################################
 
-BISON_VERSION = 3.8.2
+BISON_VERSION = 3.7.1
 BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPL-3.0+
 BISON_LICENSE_FILES = COPYING
-BISON_CPE_ID_VENDOR = gnu
 # parallel build issue in examples/c/reccalc/
 BISON_MAKE = $(MAKE1)
 HOST_BISON_DEPENDENCIES = host-m4
 HOST_BISON_CONF_OPTS = --enable-relocatable
 HOST_BISON_CONF_ENV = ac_cv_libtextstyle=no
+
+# This should be removed on uprev
+HOST_BISON_CONF_ENV += CFLAGS="$(HOST_CFLAGS) -std=gnu17"
 
 $(eval $(host-autotools-package))
