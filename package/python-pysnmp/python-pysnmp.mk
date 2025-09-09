1bd05ccace70c35cd0668b9cabfb3f2dbf7363ce
Tamara Tardif <tamara@ccxtechnologies.com>
2025-09-09 14:48:32 -0400

    pull in master from buildroot, we'll use external to manage this

--- package/python-pysnmp/python-pysnmp.mk
+++ package/python-pysnmp/python-pysnmp.mk
@@ -4,14 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYSNMP_VERSION = 5.0.20
-PYTHON_PYSNMP_SOURCE = pysnmplib-$(PYTHON_PYSNMP_VERSION).tar.gz
-PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/68/a9/e5629fa7fcd53adf6333b535165c0352b7941d1ad89e79c78f083081bb0a
-PYTHON_PYSNMP_SETUP_TYPE = setuptools
-PYTHON_PYSNMP_LICENSE = BSD-3-Clause
+PYTHON_PYSNMP_VERSION = 7.1.16
+PYTHON_PYSNMP_SOURCE = pysnmp-$(PYTHON_PYSNMP_VERSION).tar.gz
+PYTHON_PYSNMP_SITE = https://files.pythonhosted.org/packages/a2/b1/c94dac49a002eed96e2cb7aeb1d148b1c98826e5cd1f6e8eebb4a20f8d49
+PYTHON_PYSNMP_SETUP_TYPE = poetry
+PYTHON_PYSNMP_LICENSE = BSD-2-Clause
 PYTHON_PYSNMP_LICENSE_FILES = LICENSE.rst
-
-PYTHON_PYSNMP_KEEP_PY_FILES += usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/pysnmp/smi/mibs/*.py
-PYTHON_PYSNMP_KEEP_PY_FILES += usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/pysnmp/smi/mibs/instances/*.py
 
 $(eval $(python-package))
