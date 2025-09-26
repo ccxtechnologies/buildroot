be06439739cba56091c8e72794dd59fab04b55a7
Tamara Tardif <tamara@ccxtechnologies.com>
2025-09-26 15:31:03 -0400

    grpcio is dependent on protobuf

--- package/python-grpcio/python-grpcio.mk
+++ package/python-grpcio/python-grpcio.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# This version depends on protobuf and python-grpcio-reflection
 PYTHON_GRPCIO_SITE = $(TOPDIR)/../thirdparty/grpc
 PYTHON_GRPCIO_SITE_METHOD = local
 PYTHON_GRPCIO_SETUP_TYPE = setuptools
@@ -12,6 +13,6 @@ PYTHON_GRPCIO_LICENSE_FILES = LICENSE
 PYTHON_GRPCIO_ENV = GRPC_PYTHON_BUILD_WITH_CYTHON=1
 PYTHON_GRPCIO_CPE_ID_VENDOR = grpc
 PYTHON_GRPCIO_CPE_ID_PRODUCT = grpc
-PYTHON_GRPCIO_CPE_ID_VERSION = 1.75.0
+PYTHON_GRPCIO_CPE_ID_VERSION = 1.70.2
 
 $(eval $(python-package))
