87129850c9b62f274601ed837bf22faab75f86af
Tamara Tardif <tamara@ccxtechnologies.com>
2025-09-26 16:21:53 -0400

    cpe vendor

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -9,8 +9,6 @@ M4_SOURCE = m4-$(M4_VERSION).tar.xz
 M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_LICENSE = GPL-3.0+
 M4_LICENSE_FILES = COPYING
-
-# This should be removed with 1.4.20
-HOST_M4_CONF_ENV = CFLAGS="$(HOST_CFLAGS) -std=gnu17"
+M4_CPE_ID_VENDOR = gnu
 
 $(eval $(host-autotools-package))
