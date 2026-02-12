42328fc5ef0885a182044cefc7d56525d19a770f
Tamara Tardif <tamara@ccxtechnologies.com>
2026-02-12 11:39:45 -0500

    uprev m4 for host build

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -4,14 +4,10 @@
 #
 ################################################################################
 
-M4_VERSION = 1.4.19
+M4_VERSION = 1.4.21
 M4_SOURCE = m4-$(M4_VERSION).tar.xz
 M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_LICENSE = GPL-3.0+
 M4_LICENSE_FILES = COPYING
-HOST_M4_CONF_OPTS = --disable-static
-
-# This should be removed with 1.4.20
-HOST_M4_CONF_ENV = CFLAGS="$(HOST_CFLAGS) -std=gnu17"
 
 $(eval $(host-autotools-package))
